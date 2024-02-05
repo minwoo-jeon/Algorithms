61ed9757335be5e2b5974b6f86b0816c0643eaf5
Min Woo Jeon <jeonminwoo2000@gmail.com>
2024-02-05 16:49:45 +0900

    [Bronze V] Title: 개수 세기, Time: 128 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main  {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
@@ -22,7 +22,8 @@ public class Main {
                 count++;
         }
         System.out.println(count);
-        bf.close();
+
     }
 
 }
+
