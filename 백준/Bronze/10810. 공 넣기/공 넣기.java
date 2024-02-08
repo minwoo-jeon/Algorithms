65fc4489896dcc152658ab602308324802a9c926
Min Woo Jeon <jeonminwoo2000@gmail.com>
2024-02-08 22:29:51 +0900

    [Bronze III] Title: 공 넣기, Time: 244 ms, Memory: 18520 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.java
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -1,26 +1,36 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
-        int[] arr = new int[N];
-        int M = sc.nextInt();
+        int n = sc.nextInt(); //바구니 개수
+        int m = sc.nextInt(); // 공을 넣는 방법의 수
 
-        for(int i = 0; i < M; i++) {
-            int I = sc.nextInt();
-            int J = sc.nextInt();
-            int K = sc.nextInt();
+        int[] arr = new int[n + 1]; // +1해주기
 
-            for(int j = I - 1; j < J; j++) {
-                arr[j] = K;
+        //3개의 정수 입력 받기(시작,끝 /공번호)
+
+        int start = 0;
+        int end = 0 ;
+        int ball = 0;
+
+        // M만큼 start, end,ball을 입력 받아야 함으로 for 문 사용해 입력받고, 공의 번호를  arr배열에 저장
+        for (int i = 0; i < m; i++) {
+            start =  sc.nextInt();
+            end = sc.nextInt();
+            ball = sc.nextInt();
+
+            //입력받은 공의 번호를 start부터 end까지 저장
+            for (int j = start; j <= end; j++) {
+                arr[j] = ball;
             }
         }
-        for(int k = 0; k < arr.length; k++) {
-            System.out.print(arr[k] + " ");
+        //바구니 안에값 출력
+        for (int i = 1; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
+
     }
 }
+
