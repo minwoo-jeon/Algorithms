6682f3269c76f5a0e21aa39e9d7e373b557564ab
Min Woo Jeon <jeonminwoo2000@gmail.com>
2024-03-28 22:58:04 +0900

    [Silver II] Title: 좌표 압축, Time: 2004 ms, Memory: 292808 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		
+		int arr[]=new int[N];
+		int sort[]=new int[N];
+		HashMap<Integer,Integer> map = new HashMap<>();
+		StringTokenizer st= new StringTokenizer(br.readLine()," ");
+		
+		for(int i=0; i<N; i++) 
+			arr[i]=Integer.parseInt(st.nextToken());
+		sort = arr.clone();
+		Arrays.sort(sort);
+		int index=0;
+		for(int i=0; i<N; i++) {
+			if(!map.containsKey(sort[i]))
+				map.put(sort[i], index++);
+		}
+		for(int i=0; i<N; i++)
+			sb.append(map.get(arr[i])+" ");
+		System.out.print(sb);
+		br.close();
+	    
+	}
+}
