2c41e2115e073d31569a975110da4542b9dc2122
Min Woo Jeon <jeonminwoo2000@gmail.com>
2024-07-10 20:20:03 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -0,0 +1,13 @@
+class Solution {
+    public double solution(int[] numbers) {
+        double sum=0.0;
+        
+        for(int i=0; i < numbers.length; i++){
+            sum += numbers[i] ;
+        }
+        
+        return sum/numbers.length;
+        
+
+    }
+}
